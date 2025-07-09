cc55b6748bef75a096b91bfb52b4114b9840976f
Bowser <164537694+bowser-2077@users.noreply.github.com>
2025-07-09 17:55:12 +0200

    Delete java/net/mcreator/brokencraft directory

--- java/net/mcreator/brokencraft/item/AmethystIngotItem.java
+++
@@ -1,9 +0,0 @@
-package net.mcreator.brokencraft.item;
-
-import net.minecraft.world.item.Item;
-
-public class AmethystIngotItem extends Item {
-	public AmethystIngotItem(Item.Properties properties) {
-		super(properties);
-	}
-}
